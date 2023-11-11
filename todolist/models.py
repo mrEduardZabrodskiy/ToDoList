ab5a8bb28eed3bbddfae76e4c362327eb80731eb
Eduard <eduard.zabrodskiy@gmail.com>
2023-11-11 15:31:59 +0100

    struture  model view serializer

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=100, null=True)
+    status = models.BooleanField(default=False)
+    
